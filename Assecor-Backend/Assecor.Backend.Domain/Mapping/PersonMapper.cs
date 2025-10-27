9ce1fe2eed7024b23f47d2c977f8eb1b2cd99e42
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 16:54:11 +0100

    add personmapper

---
+++ Assecor-Backend/Assecor.Backend.Domain/Mapping/PersonMapper.cs
@@ -0,0 +1,28 @@
+﻿using Assecor.Backend.Domain.BackendModels;
+using Assecor.Backend.Domain.DalModels;
+
+namespace Assecor.Backend.Domain.Mapping
+{
+    public static class PersonMapper
+    {
+        public static List<Person> MapFromCsvPersons(IEnumerable<CsvPerson> csvPersons)
+        {
+            var persons = csvPersons.Select(MapFromCsvPerson).ToList();
+            return persons;
+        }
+
+        public static Person MapFromCsvPerson(CsvPerson csvPerson)
+        {
+            var person = new Person()
+            {
+                Id = csvPerson.Id,
+                Name = csvPerson.Name,
+                LastName = csvPerson.LastName,
+                City = csvPerson.City,
+                Color = csvPerson.Color,
+                ZipCode = csvPerson.ZipCode
+            };
+            return person;
+        }
+    }
+}
