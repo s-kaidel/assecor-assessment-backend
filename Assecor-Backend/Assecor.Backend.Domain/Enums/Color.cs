c20b81291efef2dccfccc9191b57e9037d7c7c5e
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 15:55:21 +0100

    add color enum

---
+++ Assecor-Backend/Assecor.Backend.Domain/Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace Assecor.Backend.Domain.Enums
+{
+    public enum Color
+    {
+        Blau = 1,
+        Grün = 2,
+        Violett = 3,
+        Rot = 4,
+        Gelb = 5,
+        Türkis = 6,
+        Weiß = 7
+    }
+}
