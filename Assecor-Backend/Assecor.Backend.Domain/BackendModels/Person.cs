cc207e1feb12974919c61b092b98a34a96e4c219
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 20:02:58 +0100

    nullable properties for perosn models

--- Assecor-Backend/Assecor.Backend.Domain/BackendModels/Person.cs
+++ Assecor-Backend/Assecor.Backend.Domain/BackendModels/Person.cs
@@ -5,10 +5,10 @@ namespace Assecor.Backend.Domain.BackendModels
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public int ZipCode { get; set; }
-        public string City { get; set; } = string.Empty;
-        public Color Color { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public int? ZipCode { get; set; }
+        public string? City { get; set; } = string.Empty;
+        public Color? Color { get; set; }
     }
 }
--- Assecor-Backend/Assecor.Backend.Domain/DalModels/CsvPerson.cs
+++ Assecor-Backend/Assecor.Backend.Domain/DalModels/CsvPerson.cs
@@ -5,10 +5,10 @@ namespace Assecor.Backend.Domain.DalModels
     public class CsvPerson
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public int ZipCode { get; set; }
-        public string City { get; set; } = string.Empty;
-        public Color Color { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+        public int? ZipCode { get; set; }
+        public string? City { get; set; } = string.Empty;
+        public Color? Color { get; set; }
     }
 }
