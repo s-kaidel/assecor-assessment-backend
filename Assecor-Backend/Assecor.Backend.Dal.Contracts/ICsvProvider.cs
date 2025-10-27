d451aa3945971a09d9456aef9848a86a326f5e73
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 16:19:31 +0100

    add interface, read method

---
+++ Assecor-Backend/Assecor.Backend.Dal.Contracts/ICsvProvider.cs
@@ -0,0 +1,9 @@
+﻿using Assecor.Backend.Domain.DalModels;
+
+namespace Assecor.Backend.Dal.Contracts
+{
+    public interface ICsvProvider
+    {
+        List<CsvPerson> ReadPersonsFromCsv();
+    }
+}
--- Assecor-Backend/Assecor.Backend.Dal/Provider/CsvProvider.cs
+++ Assecor-Backend/Assecor.Backend.Dal/Provider/CsvProvider.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Globalization;
+using Assecor.Backend.Dal.Contracts;
+using Assecor.Backend.Domain.DalModels;
+using CsvHelper;
+using Microsoft.Extensions.Logging;
 
 namespace Assecor.Backend.Dal.Provider
 {
-    public class CsvProvider(ILogger<CsvProvider> logger)
+    public class CsvProvider(ILogger<CsvProvider> logger) : ICsvProvider
     {
+        public List<CsvPerson> ReadPersonsFromCsv()
+        {
+            try
+            {
+                using var reader = new StreamReader("data.csv");
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                var persons = csv.GetRecords<CsvPerson>().ToList();
+                return persons;
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"An error occurred while trying to read persons from csv: {e.Message}");
+                return new();
+            }
+        }
     }
 }
