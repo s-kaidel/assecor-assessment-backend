7ea5fb2e056a25f03fa7f60f310d48444d5e3038
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 16:58:43 +0100

    add person controller

---
+++ Assecor-Backend/Assecor.Backend.Api/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using Assecor.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assecor.Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/persons")]
+    public class PersonController(IPersonService personService) : ControllerBase
+    {
+        public IActionResult GetPersons()
+        {
+            var persons = personService.GetPersons();
+            return new OkObjectResult(persons);
+        }
+    }
+}
