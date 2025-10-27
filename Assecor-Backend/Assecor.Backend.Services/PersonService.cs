7e59de86154ffaade5da4fffc53dabf5a3cfae79
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 16:54:31 +0100

    GetPersons service method

--- Assecor-Backend/Assecor.Backend.Services/PersonService.cs
+++ Assecor-Backend/Assecor.Backend.Services/PersonService.cs
@@ -1,9 +1,18 @@
-﻿using Assecor.Services.Contracts;
+﻿using Assecor.Backend.Dal.Contracts;
+using Assecor.Backend.Domain.BackendModels;
+using Assecor.Backend.Domain.Mapping;
+using Assecor.Services.Contracts;
+using Microsoft.Extensions.Logging;
 
 namespace Assecor.Backend.Services
 {
-    public class PersonService : IPersonService
+    public class PersonService(ILogger<PersonService> logger, ICsvProvider csvProvider) : IPersonService
     {
-
+        public List<Person> GetPersons()
+        {
+            var dalPersons = csvProvider.ReadPersonsFromCsv();
+            var persons = PersonMapper.MapFromCsvPersons(dalPersons);
+            return persons;
+        }
     }
 }
