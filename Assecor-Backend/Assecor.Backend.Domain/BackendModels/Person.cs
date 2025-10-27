0031f5b4e1483511dd556fdbdd4db7bf009994c4
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 15:45:52 +0100

    add domain project , person models

---
+++ Assecor-Backend/Assecor.Backend.Domain/BackendModels/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Assecor.Backend.Domain.BackendModels
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int ZipCode { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
---
+++ Assecor-Backend/Assecor.Backend.Domain/DalModels/DbPerson.cs
@@ -0,0 +1,12 @@
+﻿namespace Assecor.Backend.Domain.DalModels
+{
+    public class DbPerson
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int ZipCode { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
