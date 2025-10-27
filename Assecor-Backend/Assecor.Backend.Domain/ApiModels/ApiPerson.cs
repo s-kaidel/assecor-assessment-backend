160c290be775a90e675b58ea3dc4bb807520f5e6
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 20:02:47 +0100

    api person, mappings

---
+++ Assecor-Backend/Assecor.Backend.Domain/ApiModels/ApiPerson.cs
@@ -0,0 +1,14 @@
+﻿using Assecor.Backend.Domain.Enums;
+
+namespace Assecor.Backend.Domain.ApiModels
+{
+    public class ApiPerson
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int? ZipCode { get; set; }
+        public string? City { get; set; }
+        public Color? Color { get; set; }
+    }
+}
---
+++ Assecor-Backend/Assecor.Backend.Domain/Mapping/ApiPersonMapper.cs
@@ -0,0 +1,28 @@
+﻿using Assecor.Backend.Domain.ApiModels;
+using Assecor.Backend.Domain.BackendModels;
+
+namespace Assecor.Backend.Domain.Mapping
+{
+    public static class ApiPersonMapper
+    {
+        public static List<ApiPerson> MapFromDomainPersons(IEnumerable<Person> domainPersons)
+        {
+            var persons = domainPersons.Select(MapFromDomainPerson).ToList();
+            return persons;
+        }
+
+        public static ApiPerson MapFromDomainPerson(Person domainPerson)
+        {
+            var person = new ApiPerson()
+            {
+                Id = domainPerson.Id,
+                Name = domainPerson.Name,
+                LastName = domainPerson.LastName,
+                City = domainPerson.City,
+                Color = domainPerson.Color,
+                ZipCode = domainPerson.ZipCode
+            };
+            return person;
+        }
+    }
+}
