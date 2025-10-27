40185a794d83d8a9b81ddf29a4b1296610191208
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-27 16:19:13 +0100

    add csv person model

---
+++ Assecor-Backend/Assecor.Backend.Domain/DalModels/CsvPerson.cs
@@ -0,0 +1,14 @@
+﻿using Assecor.Backend.Domain.Enums;
+
+namespace Assecor.Backend.Domain.DalModels
+{
+    public class CsvPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int ZipCode { get; set; }
+        public string City { get; set; } = string.Empty;
+        public Color Color { get; set; }
+    }
+}
