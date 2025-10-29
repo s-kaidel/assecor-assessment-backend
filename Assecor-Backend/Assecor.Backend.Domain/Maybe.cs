2c7078d8170445b668717a6be7bf9dbb3a38b1b3
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-29 15:28:41 +0100

    add maybe struct

---
+++ Assecor-Backend/Assecor.Backend.Domain/Maybe.cs
@@ -0,0 +1,31 @@
+﻿namespace Assecor.Backend.Domain
+{
+    public readonly struct Maybe<T>
+    {
+        private readonly T _value;
+        public bool HasValue { get; }
+
+        public T Value => HasValue 
+            ? _value 
+            : throw new InvalidOperationException("Maybe has no value");
+
+        private Maybe(T value)
+        {
+            _value = value;
+            HasValue = true;
+        }
+
+        public static Maybe<T> From(T? value) => value != null ? new Maybe<T>(value) : None();
+
+        private static Maybe<T> None() => new();
+
+        public Maybe<TOut> Map<TOut>(Func<T, TOut> mappingFunc)
+        {
+            return mappingFunc == null 
+                ? throw new ArgumentNullException(nameof(mappingFunc)) 
+                : HasValue 
+                    ? new Maybe<TOut>(mappingFunc(_value)) 
+                    : Maybe<TOut>.None();
+        }
+    }
+}
