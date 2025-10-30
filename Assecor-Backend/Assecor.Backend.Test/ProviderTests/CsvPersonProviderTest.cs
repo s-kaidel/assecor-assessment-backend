03ddf328dfe6123ebe247a9d6e1d595170432345
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-30 11:42:15 +0100

    fix provider test

--- Assecor-Backend/Assecor.Backend.Test/ProviderTests/CsvPersonProviderTest.cs
+++ Assecor-Backend/Assecor.Backend.Test/ProviderTests/CsvPersonProviderTest.cs
@@ -12,19 +12,22 @@ namespace Assecor.Backend.Test.ProviderTests
     public class CsvPersonProviderTest
     {
         private readonly ICsvPersonProvider _sut;
+        private readonly CsvSettings _settings = new() { Files = new() };
         private readonly ICsvReader<CsvPerson> _readerMock = Substitute.For<ICsvReader<CsvPerson>>();
         private readonly ILogger<CsvPersonProvider> _loggerMock = Substitute.For<ILogger<CsvPersonProvider>>();
-        private CsvSettings _settings = new();
         private readonly IOptions<CsvSettings> _settingsMock = Substitute.For<IOptions<CsvSettings>>();
         private readonly ICsvFileLocationHandler _fileHandlerMock = Substitute.For<ICsvFileLocationHandler>();
 
         public CsvPersonProviderTest()
         { 
+            SetupCsvMocks();
             _sut = new CsvPersonProvider(_loggerMock, _readerMock, _fileHandlerMock, _settingsMock);
         }
 
-        private void SetupReaderMock()
+        private void SetupCsvMocks()
         {
+            _settingsMock.Value.Returns(_settings);
+            _fileHandlerMock.GetCsvFilePath(Arg.Any<string>()).Returns(string.Empty);
             _readerMock
             .ReadFromCsvAsync(Arg.Any<Func<IEnumerable<string>, int, CsvPerson>>(), Arg.Any<string>())
             .Returns(Task.FromResult(_persons));
@@ -77,8 +80,6 @@ namespace Assecor.Backend.Test.ProviderTests
         public async Task GetAllPersons_Should_Return_All_Persons()
         {
             var expectedCount =_persons.Count;
-            SetupReaderMock();
-            
 
             var result = await _sut.GetAllPersonsAsync();
 
@@ -89,7 +90,6 @@ namespace Assecor.Backend.Test.ProviderTests
         public async Task GetAllPersons_Should_Log_Person_Count()
         {
             var expectedLogMessage = $"Found {_persons.Count} person";
-            SetupReaderMock();
 
             await _sut.GetAllPersonsAsync();
 
@@ -106,7 +106,6 @@ namespace Assecor.Backend.Test.ProviderTests
         [InlineData(Color.Weiß, 0)]
         public async Task GetPersonsByColor_Should_Return_Persons_By_Color(Color queryColor, int expectedCount)
         {
-            SetupReaderMock();
             var result = await _sut.GetPersonsByColorAsync(queryColor);
             result.Count.ShouldBe(expectedCount);
         }
@@ -117,7 +116,7 @@ namespace Assecor.Backend.Test.ProviderTests
         public async Task GetPersonsByColor_Should_Log_Count(Color queryColor, int expectedCount)
         {
             var expectedLogMessage = $"Found {expectedCount} persons for color '{queryColor.ToString()}'";
-            SetupReaderMock();
+            
             await _sut.GetPersonsByColorAsync(queryColor);
 
             _loggerMock.Received().Log(
@@ -133,7 +132,6 @@ namespace Assecor.Backend.Test.ProviderTests
         {
             var expectedName = "Max";
             var id = 1;
-            SetupReaderMock();
 
             var result = await _sut.GetPersonByIdAsync(id);
             result.HasValue.ShouldBeTrue();
@@ -143,9 +141,7 @@ namespace Assecor.Backend.Test.ProviderTests
         [Fact]
         public async Task GetById_Should_Return_Maybe_None()
         {
-            
             var id = 123;
-            SetupReaderMock();
 
             var result = await _sut.GetPersonByIdAsync(id);
             result.HasValue.ShouldBeFalse();
@@ -157,8 +153,6 @@ namespace Assecor.Backend.Test.ProviderTests
             var id = 123;
             var expectedLogMessage = $"No entity of type '{nameof(CsvPerson)}' with key '{id}' found!";
             
-            SetupReaderMock();
-
             await _sut.GetPersonByIdAsync(id);
 
             _loggerMock.Received().Log(
