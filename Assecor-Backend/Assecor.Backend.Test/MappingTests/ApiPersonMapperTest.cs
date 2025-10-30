f09ca0a7b1b4e3c5cf1e66522bc1c50fc99f574f
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-30 14:56:17 +0100

    add mapper tests

---
+++ Assecor-Backend/Assecor.Backend.Test/MappingTests/ApiPersonMapperTest.cs
@@ -0,0 +1,63 @@
+﻿using Assecor.Backend.Domain.BackendModels;
+using Assecor.Backend.Domain.Enums;
+using Assecor.Backend.Domain.Mapping;
+
+namespace Assecor.Backend.Test.MappingTests
+{
+    public class ApiPersonMapperTest
+    {
+        [Fact]
+        public void Should_Map_Correct_Person()
+        {
+            var person = new Person()
+            {
+                Id = 1,
+                Name = "Hans",
+                LastName = "Habicht",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Rot
+            };
+
+            var apiPerson = ApiPersonMapper.MapFromDomainPerson(person);
+
+            var expectedColor = person.Color.ToString()?.ToLower();
+
+            apiPerson.Id.ShouldBe(person.Id);
+            apiPerson.Name.ShouldBe(person.Name);
+            apiPerson.LastName.ShouldBe(person.LastName);
+            apiPerson.City.ShouldBe(person.City);
+            apiPerson.ZipCode.ShouldBe(person.ZipCode);
+            apiPerson.Color.ShouldBe(expectedColor);
+        }
+
+        [Fact]
+        public void Should_Map_All_Persons()
+        {
+            var person1 = new Person()
+            {
+                Id = 1,
+                Name = "Hans",
+                LastName = "Habicht",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Rot
+            };
+
+            var person2 = new Person()
+            {
+                Id = 2,
+                Name = "Gundula",
+                LastName = "Geier",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Blau
+            };
+
+            var expectedCount = 2;
+
+            var persons = ApiPersonMapper.MapFromDomainPersons([person1, person2]);
+            persons.Count.ShouldBe(expectedCount);
+        }
+    }
+}
---
+++ Assecor-Backend/Assecor.Backend.Test/MappingTests/PersonMapperTest.cs
@@ -0,0 +1,62 @@
+﻿using Assecor.Backend.Domain.BackendModels;
+using Assecor.Backend.Domain.DalModels;
+using Assecor.Backend.Domain.Enums;
+using Assecor.Backend.Domain.Mapping;
+
+namespace Assecor.Backend.Test.MappingTests
+{
+    public class PersonMapperTest
+    {
+        [Fact]
+        public void Should_Map_Correct_Person()
+        {
+            var csvPerson = new CsvPerson()
+            {
+                Id = 1,
+                Name = "Hans",
+                LastName = "Habicht",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Rot
+            };
+
+            var person = PersonMapper.MapFromCsvPerson(csvPerson);
+
+            person.Id.ShouldBe(csvPerson.Id);
+            person.Name.ShouldBe(csvPerson.Name);
+            person.LastName.ShouldBe(csvPerson.LastName);
+            person.City.ShouldBe(csvPerson.City);
+            person.ZipCode.ShouldBe(csvPerson.ZipCode);
+            person.Color.ShouldBe(csvPerson.Color);
+        }
+
+        [Fact]
+        public void Should_Map_All_Persons()
+        {
+            var csvPerson1 = new CsvPerson()
+            {
+                Id = 1,
+                Name = "Hans",
+                LastName = "Habicht",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Rot
+            };
+
+            var csvPerson2 = new CsvPerson()
+            {
+                Id = 2,
+                Name = "Gundula",
+                LastName = "Geier",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Blau
+            };
+
+            var expectedCount = 2;
+
+            var persons = PersonMapper.MapFromCsvPersons([csvPerson1, csvPerson2]);
+            persons.Count.ShouldBe(expectedCount);
+        }
+    }
+}
