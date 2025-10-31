c8200cd464ffaf54248f61eb5f76a089385f8506
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-31 13:59:47 +0100

    better param name, check if file has records, to avoid empty line generation

--- Assecor-Backend/Assecor.Backend.Csv/CsvWriter.cs
+++ Assecor-Backend/Assecor.Backend.Csv/CsvWriter.cs
@@ -11,28 +11,41 @@ namespace Assecor.Backend.CsvAccess
         /// Appends records to a csv file, headers are ignored
         /// </summary>
         /// <param name="filePath">path to the csv file</param>
-        /// <param name="data">the objects to append</param>
+        /// <param name="records">the objects to append</param>
         /// <returns></returns>
         /// <exception cref="CsvWriterException"></exception>
-        public async Task AppendToCsvAsync(string filePath, IEnumerable<T> data)
+        public async Task AppendToCsvAsync(string filePath, IEnumerable<T> records)
         {
             try
             {
+                var hasRecords = FileHasRecords(filePath);
                 var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    HasHeaderRecord = false // prohibit writing of duplicate header
+                    HasHeaderRecord = false, // prohibit writing of duplicate header
+                    Delimiter = ", " //add whitespace to delimiter
                 };
 
                 await using var stream = new FileStream(filePath, FileMode.Append, FileAccess.Write);
                 await using var writer = new StreamWriter(stream);
                 await using var csv = new CsvHelper.CsvWriter(writer, config);
 
-                await csv.WriteRecordsAsync(data);
+                if (hasRecords)
+                {
+                    //skip to next line to avoid writing into an existing row/record
+                    await csv.NextRecordAsync();
+                }
+                await csv.WriteRecordsAsync(records);
             }
             catch (Exception ex)
             {
                 throw new CsvWriterException($"An error occurred while trying to write data to the csv file: {ex}");
             }
+        }
+
+        private static bool FileHasRecords(string filePath)
+        {
+            var hasRecords = File.ReadLines(filePath).Skip(1).Any();
+            return hasRecords;
         }
     }
 }
