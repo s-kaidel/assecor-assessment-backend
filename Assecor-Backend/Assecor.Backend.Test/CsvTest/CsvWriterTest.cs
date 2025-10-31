27a30edaa53b9050354f5b0ae87fcc85b7dc6761
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-31 13:04:06 +0100

    rename test folder, add writer test

---
+++ Assecor-Backend/Assecor.Backend.Test/CsvTest/CsvWriterTest.cs
@@ -0,0 +1,26 @@
+﻿using Assecor.Backend.CsvAccess;
+using Assecor.Backend.CsvAccess.Interfaces;
+using Assecor.Backend.Domain.DalModels;
+using Assecor.Backend.Domain.Enums;
+
+namespace Assecor.Backend.Test.CsvTest
+{
+    public class CsvWriterTest
+    {
+        private readonly ICsvWriter<CsvPerson> _sut = new CsvWriter<CsvPerson>();
+
+        [Fact]
+        public async Task Should_Append_To_Csv()
+        {
+            var csvPerson = new CsvPerson()
+            {
+                Id = 1,
+                Name = "Hans",
+                LastName = "Habicht",
+                City = "Falkendorf",
+                ZipCode = 91074,
+                Color = Color.Rot
+            };
+        }
+    }
+}
