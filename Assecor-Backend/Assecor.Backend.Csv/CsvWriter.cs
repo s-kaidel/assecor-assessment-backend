5ab9b5cf9482a8be1c3731babc1e97bc378586ba
s-kaidel <sebastian.kaidel@gmail.com>
2025-10-31 14:23:42 +0100

    update summary, add more writing options

--- Assecor-Backend/Assecor.Backend.Csv/CsvWriter.cs
+++ Assecor-Backend/Assecor.Backend.Csv/CsvWriter.cs
@@ -8,20 +8,22 @@ namespace Assecor.Backend.CsvAccess
     public class CsvWriter<T> : ICsvWriter<T>
     {
         /// <summary>
-        /// Appends records to a csv file, headers are ignored
+        /// Appends records to a csv file
         /// </summary>
         /// <param name="filePath">path to the csv file</param>
         /// <param name="records">the objects to append</param>
+        /// <param name="fileHasHeaderRow">to signal if file has an existing header row, defaults to false</param>
+        /// <param name="writeHeaderRow">if true, an auto-generated header row matching the types property names will be written. defaults to false</param>
         /// <returns></returns>
         /// <exception cref="CsvWriterException"></exception>
-        public async Task AppendToCsvAsync(string filePath, IEnumerable<T> records)
+        public async Task AppendToCsvAsync(string filePath, IEnumerable<T> records, bool fileHasHeaderRow = false, bool writeHeaderRow = false)
         {
             try
             {
                 var hasRecords = FileHasRecords(filePath);
                 var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    HasHeaderRecord = false, // prohibit writing of duplicate header
+                    HasHeaderRecord = writeHeaderRow, //false prohibits writing of duplicate header row
                     Delimiter = ", " //add whitespace to delimiter
                 };
 
@@ -29,7 +31,7 @@ namespace Assecor.Backend.CsvAccess
                 await using var writer = new StreamWriter(stream);
                 await using var csv = new CsvHelper.CsvWriter(writer, config);
 
-                if (hasRecords)
+                if (hasRecords || fileHasHeaderRow)
                 {
                     //skip to next line to avoid writing into an existing row/record
                     await csv.NextRecordAsync();
@@ -44,7 +46,7 @@ namespace Assecor.Backend.CsvAccess
 
         private static bool FileHasRecords(string filePath)
         {
-            var hasRecords = File.ReadLines(filePath).Skip(1).Any();
+            var hasRecords =  File.ReadLines(filePath).Any();
             return hasRecords;
         }
     }
--- Assecor-Backend/Assecor.Backend.Csv/Interfaces/ICsvWriter.cs
+++ Assecor-Backend/Assecor.Backend.Csv/Interfaces/ICsvWriter.cs
@@ -2,6 +2,14 @@
 {
     public interface ICsvWriter<in T>
     {
-        Task AppendToCsvAsync(string filePath, IEnumerable<T> data);
+        /// <summary>
+        /// Appends records to a csv file
+        /// </summary>
+        /// <param name="filePath">path to the csv file</param>
+        /// <param name="records">the objects to append</param>
+        /// <param name="fileHasHeaderRow">to signal if file has an existing header row, defaults to false</param>
+        /// <param name="writeHeaderRow">if true, an auto-generated header row matching the types property names will be written. defaults to false</param>
+        /// <returns></returns>
+        Task AppendToCsvAsync(string filePath, IEnumerable<T> records, bool fileHasHeaderRow = false, bool writeHeaderRow = false);
     }
 }
